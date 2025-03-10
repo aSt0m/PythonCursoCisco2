ec169ffbc9796ce4f7da88c213a6a0b1767ced78
ast0m <gavomaza1@gmail.com>
2025-03-10 01:51:47 -0600

    Primer commit

---
+++ Modulo_1/modulos_y_paquetes/modulos_paquetes_PIP.py
@@ -0,0 +1,71 @@
+
+# import math as ma
+
+# print(ma.pi)
+# print(ma.e)
+
+
+# from math import pi as p, sin as sen 
+
+# print(sen(p/2))
+
+# import math
+# import datetime
+# print(dir(math))
+# print("datetime " , dir(datetime))
+
+# for element in dir(math):
+#         print(element, end="\n")
+    
+    
+print("------modulo platform-------")
+
+# from platform import platform
+# import sys
+# import os
+
+# print(platform())
+# print(platform(1))
+# print(platform(0, 1))
+
+# print(os.name)
+# print(os.path)
+# print(sys.platform)
+
+
+print("------ PIP paquetes pygame ------" )
+
+# import pygame
+
+# run = True
+# width = 700
+# height = 200
+# pygame.init()
+# screen = pygame.display.set_mode((width, height))
+# font = pygame.font.SysFont(None, 48)
+# text = font.render("Bienvenido a Kali", True, (255, 255, 255))
+# screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+# pygame.display.flip()
+# while run:
+#   for event in pygame.event.get():
+#    if event.type == pygame.QUIT\
+#    or event.type == pygame.MOUSEBUTTONUP\
+#    or event.type == pygame.KEYUP:
+#     run = False
+
+
+
+print("pregunta")
+
+import math
+ 
+result = math.e != math.pow(2, 4)
+print(int(result))
+
+print("Pregunta")
+
+from random import randint
+    
+for i in range(2):
+   print(randint(1, 2), end='')
+    
---
+++ Modulo_2/cadenas_metodos-de-cadenas_listas_excepciones.py
@@ -0,0 +1,18 @@
+
+
+"""
+Este archivo contiene los ejercicios de las seccion 1 del modulo 2 del curso de python nivel intermedio
+"""
+
+
+print("------- Operaciones con cadenas --------")
+print("")
+
+
+# chr(ord(x)) == x
+# ord(chr(x)) == x
+
+s1 = "¿Dónde están las nieves de antaño?"
+s2 = s1.split()
+print(s2)
+print(s2[-2])
